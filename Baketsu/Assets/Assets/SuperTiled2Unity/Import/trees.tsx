d1a1ac148b1aa6cda6dc8f002bc14c88dc89a312
mehrlean <merlin.abesha@outlook.de>
2021-05-26 23:05:25 +0200

    Collision fix
    Collision ist jetzt nicht mehr an die Sprites gekoppelt, sondern sind jetzt einzelne Object Layer.

--- Baketsu/Assets/Assets/SuperTiled2Unity/Import/trees.tsx
+++ Baketsu/Assets/Assets/SuperTiled2Unity/Import/trees.tsx
@@ -8,96 +8,9 @@
    </object>
   </objectgroup>
  </tile>
- <tile id="25">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="18.6364" y="32.0909">
-    <polygon points="0,0 2.81818,-6.45455 6.72727,-13.2727 12.5455,-19.6364 13.6364,-19.0909 13.3636,-0.181818"/>
-   </object>
-  </objectgroup>
- </tile>
- <tile id="26">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0.0909091" y="12.5455" width="31.9091" height="19.4545"/>
-   <object id="2" x="-0.181818" y="12.5455">
-    <polygon points="0,0 5.27273,-1.09091 1.18182,-3.09091 1.90909,-7.27273 6.27273,-6.90909 4.63636,-9.45455 7.27273,-11.8182 11.0909,-12.5455 15.6364,-12.2727 18.8182,-8.72727 21.3636,-10.0909 23,-9.45455 28.1818,-4.54545 28.6364,-1.18182 32.4545,-1.45455 32.3636,0.272727"/>
-   </object>
-  </objectgroup>
- </tile>
- <tile id="27">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0.181818" y="12.1818">
-    <polygon points="0,0 5.18182,1.09091 8.81818,7.54545 8.54545,10.5455 12.7273,14.6364 15.8182,19 15.8182,19.8182 -0.272727,19.7273"/>
-   </object>
-  </objectgroup>
- </tile>
- <tile id="41">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="12.0909" y="31.9091">
-    <polygon points="0,0 -5.63636,-4.81818 -3.72727,-14.2727 0.454545,-18.5455 2.72727,-19.3636 3.90909,-23.4545 5.90909,-25.5455 8.90909,-27.5455 4.63636,-28.4545 3.81818,-29.8182 5.72727,-32.0909 19.8182,-31.8182 20.0909,0.181818"/>
-   </object>
-  </objectgroup>
- </tile>
- <tile id="43">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0.181818" y="0">
-    <polygon points="0,0 14.8182,0 20.6364,9.54545 22.1818,16.1818 18.7273,19.7273 19.9091,23.2727 18.7273,30.2727 15,31.2727 0,32.0909"/>
-   </object>
-  </objectgroup>
- </tile>
- <tile id="54">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="-0.727273" y="0" width="32.7273" height="21.3636"/>
-   <object id="2" x="9.54545" y="21.7273" width="12.1818" height="9.90909"/>
-   <object id="3" x="3.81819" y="21.4091" width="7.27271" height="3.18182"/>
-   <object id="6" x="21.8182" y="20.7727" width="2.63635" height="6.72727"/>
-  </objectgroup>
- </tile>
- <tile id="57">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="15.3636" y="-0.363636">
-    <polygon points="0,0 -3.36364,6.54545 -3.36364,11 -0.454545,15.3636 4.18182,16.5455 8.18182,17.4545 8.54545,21.6364 12.1818,22.2727 16.5455,23.2727 16.7273,0.454545"/>
-   </object>
-  </objectgroup>
- </tile>
- <tile id="58">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="32" height="32"/>
-  </objectgroup>
- </tile>
- <tile id="59">
-  <objectgroup draworder="index" id="2">
-   <object id="2" x="0.181818" y="23.1818">
-    <polygon points="0,0 10.4545,-3.27273 17.1818,-9.63636 17.8182,-20 15.4545,-23 -0.0909091,-23.1818"/>
-   </object>
-  </objectgroup>
- </tile>
- <tile id="70">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="10" y="0" width="11" height="25"/>
-  </objectgroup>
- </tile>
- <tile id="74">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="11" y="0" width="10" height="23"/>
-  </objectgroup>
- </tile>
  <tile id="82">
   <objectgroup draworder="index" id="2">
    <object id="1" x="8.96891" y="7.24051" width="11" height="14.1522"/>
-  </objectgroup>
- </tile>
- <tile id="97">
-  <objectgroup draworder="index" id="2">
-   <object id="2" x="5.27273" y="6.72727" width="22" height="18.9091">
-    <ellipse/>
-   </object>
-  </objectgroup>
- </tile>
- <tile id="99">
-  <objectgroup draworder="index" id="2">
-   <object id="2" x="1.81818" y="3.18182" width="26.9091" height="25.8182">
-    <ellipse/>
-   </object>
   </objectgroup>
  </tile>
 </tileset>
