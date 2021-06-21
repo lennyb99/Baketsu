42332a90b62b6d471ac417909d6135eb3f02b2b0
mehrlean <merlin.abesha@outlook.de>
2021-06-21 15:25:05 +0200

    Village added
    removed Testing Maps, added Village map area

---
+++ Baketsu/Assets/Assets/SuperTiled2Unity/Import/Japanese_city.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="Japanese_city" tilewidth="32" tileheight="32" tilecount="544" columns="34">
+ <image source="pngs/Japanese_city.png" width="1088" height="512"/>
+</tileset>
--- Baketsu/Assets/Assets/SuperTiled2Unity/Import/walls.tsx
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="TX Tileset Wall" tilewidth="32" tileheight="32" tilecount="256" columns="16">
- <image source="walls.png" width="512" height="512"/>
- <tile id="148">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="32" height="32"/>
-  </objectgroup>
- </tile>
-</tileset>
