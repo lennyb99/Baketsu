c55738d581a5334909bb878e8a871116da8d4e39
mehrlean <merlin.abesha@outlook.de>
2021-06-22 18:01:54 +0200

    added overworld1 area
    added overworld1 area to main Map

---
+++ Baketsu/Assets/Assets/SuperTiled2Unity/Import/cars2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="cars2" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="../../../../../../../../../../Downloads/cars2.png" width="98" height="123"/>
+</tileset>
