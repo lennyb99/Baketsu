08eb64ffd36ac584637ce28c03009cce54d6de2d
lennyb99 <lennogle@gmail.com>
2021-06-22 23:36:13 +0200

    enemies added

---
+++ Baketsu/Assets/Scripts/Enemy/Wasp.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wasp : Enemy
+{
+    public Transform target;
+    public float chaseRadius;
+    public float attackRadius;
+    public Transform homePos;
+
+
+    private Rigidbody2D myRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = EnemyState.idle;
+        myRigidbody = GetComponent<Rigidbody2D>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        CheckDistance();
+    }
+
+   
+
+    
+
+    void CheckDistance(){
+        if(Vector3.Distance(target.position,transform.position)<= chaseRadius &&
+                Vector3.Distance(target.position, transform.position)>= attackRadius && currentState != EnemyState.stagger && currentState != EnemyState.attack){
+            Vector3 temp = Vector3.MoveTowards(transform.position,target.position, moveSpeed * Time.deltaTime);
+            myRigidbody.MovePosition(temp);
+            
+        }
+    }
+
+    private void ChangeState(EnemyState newState){
+        if(currentState != newState){
+            currentState = newState;
+        }
+    }
+}
