4fbc57ae575b661d3a0121a2036b83ff28b1a10b
lennyb99 <lennogle@gmail.com>
2021-06-23 12:56:29 +0200

    controlsmenu

---
+++ Baketsu/Assets/Scripts/Dialogue/PauseMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    
+    public GameObject controls;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+
+            if(controls.active == false){
+                controls.SetActive(true);
+            }else{
+                controls.SetActive(false);
+            }
+
+        }
+    }
+}
